f886748f23b7e2811016b1202e679fe1ee7e7287
Parrvaneh <46057565+Parrvaneh@users.noreply.github.com>
2020-05-09 00:58:55 +0300

    Update Week 01 - Exercise01 (House Prices).py

--- Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning/Week 01 - Exercise01 (House Prices).py
+++ Introduction to TensorFlow for Artificial Intelligence, Machine Learning, and Deep Learning/Week 01 - Exercise01 (House Prices).py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 ## Programming Assignment: Exercise 1 (Housing Prices)
 
 # I have to build a neural network to predict the price of a house.
@@ -11,10 +5,6 @@
 # a house costs 50k + 50k per bedroom, so that a 1 bedroom house costs 100k, a 2 bedroom house costs 150k etc.
 # We predict the price for a house with 7 bedrooms.
 # The output should be scaled down, it should predict the number 4 for 400, and then your answer is in the 'hundreds of thousands'
-
-
-# In[4]:
-
 
 import tensorflow as tf
 import numpy as np # for input the sets as arrays
@@ -26,10 +16,5 @@ ys = np.array([1.0, 1.5, 2.0, 2.5, 3.0], dtype = float)
 model.fit(xs, ys, epochs = 500)
 print(model.predict([7]))
 print('hundreds of thousands')
-
-
-# In[ ]:
-
-
 
 
